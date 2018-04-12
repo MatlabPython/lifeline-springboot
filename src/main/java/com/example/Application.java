752f61413b7a64dd2f27c956512f2bb2c120dbce
gyf0982034 <2479296995@qq.com>
2018-04-12 15:31:45 +0800

    提交

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,9 +1,7 @@
-package com.springboot.example;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: yifeng G
@@ -13,14 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @Vsersion:
  */
 @SpringBootApplication
-@RestController
 public class Application {
-    @RequestMapping("/")
-    public String home(){
-        return "go home";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
