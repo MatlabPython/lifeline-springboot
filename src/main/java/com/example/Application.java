d0618d0792b2885690fc9b568a8748200122b12b
TOSHIBA <1799906345@qq.com>
2018-04-13 17:17:18 +0800

    git代码提交

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import com.example.common.config.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @Author: yifeng G
@@ -11,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Vsersion:
  */
 @SpringBootApplication
+@Import({DynamicDataSourceRegister.class}) // 注册动态多数据源
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
